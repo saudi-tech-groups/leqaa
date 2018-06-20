012a1b6edb7a4684b527973e85da33b08a93b083
M.Aljehany <moath@moath.co>
2018-06-20 06:49:25 +0300

    rename file  VerifyMail to UserVerificationMail .

--- resources/views/emails/verification.blade.php
+++ resources/views/emails/verification.blade.php
@@ -9,11 +9,7 @@
 <br/>
 Your registered email-id is {{$user['email']}} , Please click on the below link to verify your email account
 <br/>
-<<<<<<< HEAD
 <a href="{{route('verify', $user->VerificationToken->token)}}">Verify Email</a>
-=======
-<a href="{{url('user/verify', $user->verifyUser->token)}}">Verify Email</a>
->>>>>>> cc9deab8c8b1... User registration
 </body>
 
 </html>
