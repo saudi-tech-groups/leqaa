129f1b0403a57dd207e1fabc53de4e0cda34fc9d
Ibrahim AshShohail <me@ibrasho.com>
2018-06-21 00:57:18 +0300

    add laravel horizon
    Signed-off-by: Ibrahim AshShohail <me@ibrasho.com>

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Horizon\Horizon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Horizon::auth(function ($request) {
+            return app()->environment('local')
+                || Auth::user()->id == 1;
+        });
     }
 
     /**
--- config/app.php
+++ config/app.php
@@ -210,6 +210,8 @@ return [
         'Validator'    => Illuminate\Support\Facades\Validator::class,
         'View'         => Illuminate\Support\Facades\View::class,
 
+        'Horizon'      => Laravel\Horizon\Horizon::class,
+
     ],
 
 ];
