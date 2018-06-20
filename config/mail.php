81f7d33a4ec65a3b5cbf2d57d95ec1b0eb702505
Ibrahim AshShohail <me@ibrasho.com>
2018-06-21 01:38:51 +0300

    fix send from address domain
    Signed-off-by: Ibrahim AshShohail <me@ibrasho.com>

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'mailer@leqaa.orq'),
+        'address' => env('MAIL_FROM_ADDRESS', 'mailer@leqaa.org'),
         'name'    => env('MAIL_FROM_NAME', 'لقاء - Leqaa'),
     ],
 
