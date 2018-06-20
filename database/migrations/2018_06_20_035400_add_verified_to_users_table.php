7d3e29f4c88719635121ee79c9e09493af022e57
M.Aljehany <moath@moath.co>
2018-06-20 06:55:57 +0300

    added verified .

---
+++ database/migrations/2018_06_20_035400_add_verified_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddVerifiedToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+             $table->boolean('verified')->default(false);
+             $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('verified');
+            $table->dropSoftDeletes();
+        });
+    }
+}
