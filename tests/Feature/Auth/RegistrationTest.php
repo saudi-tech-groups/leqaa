a446f90d684ed502421714c7e1763c27fc326615
Ibrahim AshShohail <me@ibrasho.com>
2018-06-21 04:35:13 +0300

    setup travis
    Signed-off-by: Ibrahim AshShohail <me@ibrasho.com>

---
+++ tests/Feature/Auth/RegistrationTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class RegistrationTest extends TestCase
+{
+    use DatabaseMigrations, RefreshDatabase;
+
+    public function testNewRegistration()
+    {
+        $password = str_random(8);
+
+        $res = $this->post('auth/register', [
+            'name'                  => 'User Name',
+            'email'                 => 'user@leqaa.org',
+            'password'              => $password,
+            'password_confirmation' => $password,
+        ]);
+
+        $res->assertStatus(200);
+    }
+}
