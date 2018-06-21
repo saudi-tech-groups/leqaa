2a325eee61e5ceb8c3dcf9d6c47bbefddf573fbc
Ibrahim AshShohail <me@ibrasho.com>
2018-06-21 04:35:13 +0300

    setup travis

---
+++ README.md
@@ -0,0 +1,9 @@
+[![Build Status](https://travis-ci.com/saudi-tech-groups/leqaa.svg?branch=master)](https://travis-ci.com/saudi-tech-groups/leqaa) [![StyleCI Status](https://github.styleci.io/repos/135148936/shield)](https://github.styleci.io/repos/135148936)
+
+# Leqaa Platform
+
+Leqaa is a meetup groups platform for communities to organize their events and activities.
+
+It's hosted on: https://leqaa.org
+
+Feel free to fork it if you need to customize it in anyway.
---
+++ tests/Feature/Auth/RegistrationTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class RegistrationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testNewRegistration()
+    {
+        $password = str_random(8);
+
+        $response = $this->post('auth/register', [
+            'name'                  => 'User Name',
+            'email'                 => 'user@leqaa.org',
+            'password'              => $password,
+            'password_confirmation' => $password,
+        ]);
+
+        $response->assertStatus(200);
+    }
+}
