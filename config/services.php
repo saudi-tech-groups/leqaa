d32a8297470d3217c5a6b8e25d30b01678156bab
MohannadNaj <me@mohannad.io>
2018-06-23 14:15:09 +0300

    SocialAuth .env variables and configurations

--- config/services.php
+++ config/services.php
@@ -35,9 +35,21 @@ return [
         'secret' => env('STRIPE_SECRET'),
     ],
 
-    'github.com' => [
+    'github' => [
         'client_id'     => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect'      => 'http://your-callback-url',
+    ],
+
+    'twitter' => [
+        'client_id'     => env('TWITTER_CLIENT_ID'),
+        'client_secret' => env('TWITTER_CLIENT_SECRET'),
+        'redirect'      => 'http://your-callback-url',
+    ],
+
+    'google' => [
+        'client_id'     => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
         'redirect'      => 'http://your-callback-url',
     ],
 
